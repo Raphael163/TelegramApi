acafacdcb507b9f8bb45b35f61869ea2c526bbd9
Raphael <102540442+Raphael163@users.noreply.github.com>
2022-07-02 16:29:50 +0300

    Update api.py

--- api.py
+++ api.py
@@ -1,2 +1,2 @@
-token_weather = '719sfasf9easfa3d01a7basasff777505asf7easfasasff2b8173122d02' # Токен погоды
-token_bot = "5129sfasf788asfas35fa5:AAFLKsf_ABR5pFGcsssadasdf5vj32D01uM-wEKAJ1uZBU" # Токен бота
+token_weather = '' # Токен погоды
+token_bot = "" # Токен бота
